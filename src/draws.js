113ceb68e755f8c8e0f2ce00b860dec02eea52c3
swimyoung <swimyoung@gmail.com>
2019-05-01 17:11:07 +0900

    add box dimension visualization

--- src/draws.js
+++ src/draws.js
@@ -4,6 +4,7 @@ export function drawCoordinate(canvas, unit, color, withUnitText = false) {
 
   ctx.clearRect(0, 0, width, height);
   ctx.beginPath();
+  ctx.save();
   ctx.lineWidth = 1;
   ctx.strokeStyle = color;
   ctx.font = `12px 'Courier New', Courier, monospace`;
@@ -38,6 +39,7 @@ export function drawCoordinate(canvas, unit, color, withUnitText = false) {
   });
 
   ctx.stroke();
+  ctx.restore();
 }
 
 export function drawWindowDimension(canvas) {
@@ -46,14 +48,148 @@ export function drawWindowDimension(canvas) {
   const { innerWidth, innerHeight, pageXOffset, pageYOffset } = window;
 
   ctx.clearRect(0, 0, width, height);
-  ctx.beginPath();
+  ctx.save();
   ctx.lineWidth = 1;
   ctx.strokeStyle = '#3d7e9a';
   ctx.font = `14px 'Courier New', Courier, monospace`;
+  const x = pageXOffset + 30;
+  ctx.fillText(`innerWidth:${innerWidth}`, x, pageYOffset + 35);
+  ctx.fillText(`innerHeight:${innerHeight}`, x, pageYOffset + 50);
+  ctx.fillText(`pageXOffset:${pageXOffset}`, x, pageYOffset + 65);
+  ctx.fillText(`pageYoffset:${pageYOffset}`, x, pageYOffset + 80);
+  ctx.restore();
+}
 
-  const x = pageXOffset + innerWidth / 2 - 100;
-  ctx.fillText(`innerWidth: ${innerWidth}`, x, pageYOffset + 35);
-  ctx.fillText(`innerHeight: ${innerHeight}`, x, pageYOffset + 50);
-  ctx.fillText(`pageXOffset: ${pageXOffset}`, x, pageYOffset + 65);
-  ctx.fillText(`pageYoffset: ${pageYOffset}`, x, pageYOffset + 80);
+export function drawBoxesDimension(canvas, boxOrBoxes) {
+  const boxes = [].concat(boxOrBoxes);
+  const { width, height } = canvas;
+  const { pageXOffset, pageYOffset } = window;
+  const ctx = canvas.getContext('2d');
+
+  ctx.clearRect(0, 0, width, height);
+  ctx.save();
+  const fontSize = 14;
+  ctx.font = `${fontSize}px 'Courier New', Courier, monospace`;
+
+  boxes.forEach(box => {
+    const {
+      x: clientRectX,
+      y: clientRectY,
+      width,
+      height,
+    } = box.getBoundingClientRect();
+    const { clientWidth, clientHeight, clientTop, clientLeft } = box;
+    const [border, margin, padding] = Object.entries(
+      window.getComputedStyle(box),
+    )
+      .filter(([key]) => ['border', 'margin', 'padding'].indexOf(key) !== -1)
+      .map(([, value]) => parseInt(value));
+
+    const x = clientRectX + pageXOffset;
+    const y = clientRectY + pageYOffset;
+
+    // margin edge
+    ctx.fillStyle = 'rgba(88, 46, 155, 0.2)';
+    ctx.fillRect(
+      x - margin,
+      y - margin,
+      width + margin * 2,
+      height + margin * 2,
+    );
+    ctx.fillStyle = '#000000';
+    ctx.fillText(`margin:${margin}px`, x - margin, y - margin);
+
+    // border edge
+    ctx.fillStyle = 'rgba(88, 46, 155, 0.2)';
+    ctx.fillRect(x, y, width, height);
+    ctx.fillStyle = '#000000';
+    ctx.fillText(`border:${border}px`, x, y);
+
+    // clientTop & clientLeft
+    ctx.beginPath();
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.moveTo(x + width - border, y);
+    ctx.lineTo(x + width - border, y + border);
+    ctx.moveTo(x, y + border + height - border * 2);
+    ctx.lineTo(x + border, y + border + height - border * 2);
+    ctx.stroke();
+    ctx.fillText(`clientTop:${clientTop}px`, x + width - border, y - 2);
+    ctx.fillText(
+      `clientLeft:${clientLeft}px`,
+      x - 90 - border,
+      y + border + height - border * 2,
+    );
+
+    // padding edge
+    ctx.fillStyle = 'rgba(88, 46, 155, 0.2)';
+    ctx.fillRect(
+      x + border,
+      y + border,
+      width - border * 2,
+      height - border * 2,
+    );
+    ctx.fillStyle = '#000000';
+    ctx.fillText(`padding:${padding}px`, x + border, y + border - 2);
+
+    // clientWidth & clientHeight
+    ctx.strokeStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.strokeRect(
+      x + border,
+      y + border,
+      width - border * 2,
+      height - border * 2,
+    );
+    ctx.fillStyle = '#000000';
+    ctx.fillText(
+      `clientWidth:${clientWidth}px`,
+      x + border,
+      y + border + height - border * 2 + fontSize,
+    );
+    ctx.fillText(
+      `clientHeight:${clientHeight}px`,
+      x + border,
+      y + border + height - border * 2 + fontSize * 2,
+    );
+
+    // content
+    ctx.fillStyle = 'rgba(88, 46, 155, 0.2)';
+    ctx.fillRect(
+      x + padding + border,
+      y + padding + border,
+      width - (padding + border) * 2,
+      height - (padding + border) * 2,
+    );
+    ctx.fillStyle = '#000000';
+    ctx.fillText(
+      `w:${width - (padding + border) * 2}px`,
+      x + padding + border,
+      y + padding + border,
+    );
+    ctx.fillText(
+      `h:${height - (padding + border) * 2}px`,
+      x + padding + border,
+      y + padding + border + fontSize,
+    );
+
+    // x, y of getBoundingClientRect
+    ctx.strokeStyle = 'rgba(88, 46, 155, 0.7)';
+    ctx.beginPath();
+    ctx.moveTo(pageXOffset, y);
+    ctx.lineTo(pageXOffset + clientRectX, y);
+    ctx.moveTo(x, pageYOffset);
+    ctx.lineTo(x, pageYOffset + clientRectY);
+    ctx.stroke();
+    ctx.fillText(
+      `clientRectX:${clientRectX}`,
+      pageXOffset + clientRectX - 140 - margin,
+      y - margin,
+    );
+    ctx.fillText(
+      `clientRectY:${clientRectY}`,
+      pageXOffset + clientRectX - 140 - margin,
+      y - margin + fontSize,
+    );
+  });
+
+  ctx.restore();
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 import './index.scss';
-import { drawCoordinate, drawWindowDimension } from './draws';
+import {
+  drawCoordinate,
+  drawWindowDimension,
+  drawBoxesDimension,
+} from './draws';
+
+const BOX_COUNT = 1;
 
 (async () => {
   await new Promise(resolve => window.addEventListener('load', resolve));
 
   const canvases = Array.from(document.getElementsByTagName('canvas'));
-  const elemSize = document.getElementById('size');
-
-  let { width, height } = window.getComputedStyle(elemSize);
-  width = parseInt(width);
-  height = parseInt(height);
-
-  canvases.forEach(canvas => {
-    canvas.setAttribute('width', width);
-    canvas.setAttribute('height', height);
-  });
-
   const [
     unitTenCoordinateCanvas,
     unitHundredCoordinateCanvas,
     windowDimensionCanvas,
+    boxDimensionCanvas,
   ] = canvases;
+
+  let boundingRect;
+  {
+    const elemSize = document.getElementById('size');
+    const { width, height } = window.getComputedStyle(elemSize);
+    boundingRect = { width: parseInt(width), height: parseInt(height) };
+  }
+  const handleBoxMove = () => {
+    drawBoxesDimension(boxDimensionCanvas, boxes);
+  };
+  const boxes = Array.from({ length: BOX_COUNT }).map(() =>
+    renderBox(boundingRect, handleBoxMove),
+  );
+
+  const { width, height } = boundingRect;
+  canvases.forEach(
+    canvas =>
+      canvas.setAttribute('width', width) ||
+      canvas.setAttribute('height', height),
+  );
+
   drawCoordinate(unitTenCoordinateCanvas, 10, '#eeeeee');
   drawCoordinate(unitHundredCoordinateCanvas, 100, '#777777', true);
   drawWindowDimension(windowDimensionCanvas);
+  drawBoxesDimension(boxDimensionCanvas, boxes);
 
   window.addEventListener('resize', () => {
     drawWindowDimension(windowDimensionCanvas);
@@ -33,3 +51,56 @@ import { drawCoordinate, drawWindowDimension } from './draws';
     drawWindowDimension(windowDimensionCanvas);
   });
 })();
+
+function renderBox(boundingRect, callback) {
+  const element = document.createElement('div');
+  element.style = `
+      position: absolute;
+      width: 100px;
+      height: 100px;
+      margin: 30px;
+      border: 40px solid rgba(0, 0, 0, 0);
+      padding: 50px;
+      transform: translate(${Math.floor(
+        Math.random() * window.innerWidth,
+      )}px, ${Math.floor(Math.random() * window.innerHeight)}px)
+    `;
+
+  element.addEventListener('mousedown', event => {
+    const { x, y, width, height } = element.getBoundingClientRect();
+    const [margin] = Object.entries(window.getComputedStyle(element))
+      .filter(([key]) => ['margin'].indexOf(key) !== -1)
+      .map(([, value]) => parseInt(value));
+
+    const { pageX: pageXOfMouseDown, pageY: pageYOfMouseDown } = event;
+    const baseX = x + window.pageXOffset - margin - pageXOfMouseDown;
+    const baseY = y + window.pageYOffset - margin - pageYOfMouseDown;
+
+    const handleWindowMouseMove = event => {
+      const { pageX, pageY } = event;
+      let x = baseX + pageX;
+      let y = baseY + pageY;
+
+      if (x + width > boundingRect.width) x = boundingRect.width - width;
+      else if (x < 0) x = 0;
+
+      if (y + height > boundingRect.height) y = boundingRect.height - height;
+      else if (y < 0) y = 0;
+
+      element.style.transform = `translate(${x}px, ${y}px)`;
+      callback(element);
+    };
+
+    const handleWindowMouseUp = () => {
+      window.removeEventListener('mouseup', handleWindowMouseUp);
+      window.removeEventListener('mousemove', handleWindowMouseMove);
+    };
+
+    window.addEventListener('mousemove', handleWindowMouseMove);
+    window.addEventListener('mouseup', handleWindowMouseUp);
+  });
+
+  document.body.appendChild(element);
+
+  return element;
+}
