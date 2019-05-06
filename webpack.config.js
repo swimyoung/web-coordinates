5a27a9cb9cbfbe1bdfbb80de1ecfb731b3f468d0
swimyoung <swimyoung@gmail.com>
2019-05-06 10:24:52 +0900

    remove scss

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss|\.css/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        test: /\.css/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.js[x]?$/,
