83f33c3a5ce1b627005d0b3ae7831d654ed94354
swimyoung <swimyoung@gmail.com>
2020-07-12 16:44:23 +0900

    Use svg to draw coordinate and improve performance

--- src/Coordinate.tsx
+++ src/Coordinate.tsx
@@ -1,50 +1,14 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Boundary } from './App';
 
 type CoordinateProps = Boundary;
 
-function Coordinate(props: CoordinateProps) {
+function CoordinateUnitText(props: {
+  width: number;
+  height: number;
+}): React.ReactElement {
   const { width, height } = props;
-  const canvas = useRef<HTMLCanvasElement>(null);
   const [pageOffset, setPageOffset] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const { current: element } = canvas;
-    if (!element) {
-      return;
-    }
-
-    const ctx = element.getContext('2d');
-    if (!ctx) {
-      return;
-    }
-
-    ctx.clearRect(0, 0, width, height);
-    [
-      { color: '#eeeeee', unit: 10 },
-      { color: '#cccccc', unit: 100 },
-    ].forEach(({ color, unit }) => {
-      ctx.save();
-      ctx.beginPath();
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = color;
-
-      Array.from({ length: Math.ceil(height / unit) }).forEach((_, index) => {
-        const y = index * unit;
-        ctx.moveTo(0, y);
-        ctx.lineTo(width, y);
-      });
-
-      Array.from({ length: Math.ceil(width / unit) }).forEach((_, index) => {
-        const x = index * unit;
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, height);
-      });
-
-      ctx.stroke();
-      ctx.restore();
-    });
-  }, [width, height]);
 
   useEffect(() => {
     const handleWindowScroll = () => {
@@ -57,51 +21,115 @@ function Coordinate(props: CoordinateProps) {
       window.removeEventListener('scroll', handleWindowScroll);
     };
   }, []);
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          position: 'absolute',
+          transform: `translate(0px, ${pageOffset.y}px)`,
+        }}
+      >
+        {useMemo(
+          () =>
+            Array.from({ length: width / 100 }).map((_, index) => {
+              return (
+                <span
+                  style={{
+                    position: 'absolute',
+                    display: 'inline-block',
+                    transform: `translate(${index * 100}px, 0px)`,
+                  }}
+                  key={index}
+                >
+                  {index * 100}
+                </span>
+              );
+            }),
+          [width],
+        )}
+      </div>
+      <div
+        style={{
+          position: 'absolute',
+          transform: `translate(${pageOffset.x}px, 0px)`,
+        }}
+      >
+        {useMemo(
+          () =>
+            Array.from({ length: height / 100 }).map((_, index) => {
+              return (
+                <span
+                  style={{
+                    position: 'absolute',
+                    display: 'inline-block',
+                    transform: `translate(0px, ${index * 100}px)`,
+                  }}
+                  key={index}
+                >
+                  {index * 100}
+                </span>
+              );
+            }),
+          [height],
+        )}
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Coordinate(props: CoordinateProps) {
+  const { width, height } = props;
 
   return (
     <React.Fragment>
-      <canvas
-        style={{ position: 'absolute' }}
-        width={width}
-        height={height}
-        ref={canvas}
-      />
-      {/* X coordinate unit */}
-      <div>
-        {Array.from({ length: width / 100 }).map((_, index) => {
+      <svg style={{ position: 'absolute' }} width={width} height={height}>
+        {[
+          { color: '#eeeeee', unit: 10 },
+          { color: '#cccccc', unit: 100 },
+        ].map(({ color, unit }, index) => {
           return (
-            <span
-              style={{
-                position: 'absolute',
-                color: '#aaaaaa',
-                display: 'inline-block',
-                transform: `translate(${index * 100}px, ${pageOffset.y}px)`,
-              }}
-              key={index}
-            >
-              {index * 100}
-            </span>
+            <React.Fragment key={index}>
+              {Array.from({ length: Math.ceil(height / unit) }).map(
+                (_, index) => {
+                  const y = index * unit;
+                  return (
+                    <line
+                      key={`${'0'}${y}${width}${y}`}
+                      x1={'0'}
+                      y1={y}
+                      x2={width}
+                      y2={y}
+                      style={{
+                        stroke: color,
+                        strokeWidth: '1',
+                      }}
+                    />
+                  );
+                },
+              )}
+              {Array.from({ length: Math.ceil(width / unit) }).map(
+                (_, index) => {
+                  const x = index * unit;
+                  return (
+                    <line
+                      key={`${x}${'0'}${x}${height}`}
+                      x1={x}
+                      y1={'0'}
+                      x2={x}
+                      y2={height}
+                      style={{
+                        stroke: color,
+                        strokeWidth: '1',
+                      }}
+                    />
+                  );
+                },
+              )}
+            </React.Fragment>
           );
         })}
-      </div>
-      {/* Y coordinate unit */}
-      <div>
-        {Array.from({ length: height / 100 }).map((_, index) => {
-          return (
-            <span
-              style={{
-                position: 'absolute',
-                color: '#aaaaaa',
-                display: 'inline-block',
-                transform: `translate(${pageOffset.x}px, ${index * 100}px)`,
-              }}
-              key={index}
-            >
-              {index * 100}
-            </span>
-          );
-        })}
-      </div>
+      </svg>
+      <CoordinateUnitText width={width} height={height} />
     </React.Fragment>
   );
 }
